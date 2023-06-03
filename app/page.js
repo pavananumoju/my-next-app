1fb7245e58836b5d0d0238788bb9f3d0dcf456e1
Pavan Kumar Anumoju <pavananumoju@gmail.com>
2023-06-03 22:59:22 +0530

    NextUI Nav Changes

--- app/page.js
+++ app/page.js
@@ -1,7 +1,65 @@
-import Image from 'next/image'
+'use client';
+import { Navbar, Button, Link, Text } from "@nextui-org/react";
+import { Layout } from "../nav/Layout.js";
+import { AcmeLogo } from "../nav/AcmeLogo.js";
 
-export default function Home() {
+export default function App() {
+  const collapseItems = [
+    "Features",
+    "Customers",
+    "Pricing",
+    "Company",
+    "Legal",
+    "Team",
+    "Help & Feedback",
+    "Login",
+    "Sign Up",
+  ];
+
   return (
-    <div>HELLO</div>
+    <Layout>
+      <Navbar isBordered variant="sticky">
+        <Navbar.Brand>
+          <Navbar.Toggle aria-label="toggle navigation" />
+          <AcmeLogo />
+          <Text b color="inherit">
+            CricBudz
+          </Text>
+        </Navbar.Brand>
+        <Navbar.Content enableCursorHighlight hideIn="xs" variant="underline">
+          <Navbar.Link href="#">Features</Navbar.Link>
+          <Navbar.Link isActive href="#">
+            Customers
+          </Navbar.Link>
+          <Navbar.Link href="#">Pricing</Navbar.Link>
+          <Navbar.Link href="#">Company</Navbar.Link>
+        </Navbar.Content>
+        <Navbar.Content>
+          <Navbar.Link color="inherit" href="#">
+            Login
+          </Navbar.Link>
+          <Navbar.Item>
+            <Button auto flat as={Link} href="#">
+              Sign Up
+            </Button>
+          </Navbar.Item>
+        </Navbar.Content>
+        <Navbar.Collapse disableAnimation>
+        {collapseItems.map((item, index) => (
+          <Navbar.CollapseItem key={item}>
+            <Link
+              color="inherit"
+              css={{
+                minWidth: "100%",
+              }}
+              href="#"
+            >
+              {item}
+            </Link>
+          </Navbar.CollapseItem>
+        ))}
+      </Navbar.Collapse>
+      </Navbar>
+    </Layout>
   )
 }
